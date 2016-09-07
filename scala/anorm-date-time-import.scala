8a1254d4943e879ada095e4cb2b3690b501185e7
Michael Bryzek <mbryzek@alum.mit.edu>
2016-09-07 07:07:16 -0400

    Add anorm date time import

---
+++ scala/anorm-date-time-import.scala
@@ -0,0 +1,8 @@
+/**
+  * Provides mappings for date time in anorm - fixes:
+  * 
+  * [error]  found   : (Symbol, org.joda.time.DateTime)
+  * [error]  required: anorm.NamedParameter
+  */
+
+import anorm.JodaParameterMetaData._
