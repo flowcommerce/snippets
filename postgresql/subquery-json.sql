1583aa23869f2c6a8ecc0afdeffe640a27633a99
Michael Bryzek <mike@flow.io>
2018-04-07 12:01:10 -0400

    Better query

--- postgresql/subquery-json.sql
+++ postgresql/subquery-json.sql
@@ -1,5 +1,10 @@
-select requests.id,
-       row_to_json(responses)::text as response,
-       (select to_json(array_agg(row_to_json(responses))) from responses where responses.request_id = requests.id)::text as responses
-  from requests
-  left join responses on responses.request_id = requests.id
+select
+  id,
+  description,
+  (
+   select array_agg(categories.label)
+    from allocations
+    join categories on categories.id = allocations.category_id
+   where allocations.transaction_id = transactions.id
+  ) as allocations
+from transactions;
