3723bbaa1ba8abb228644f80f9c351c9617dfc3f
Michael Bryzek <mike@flow.io>
2019-01-04 16:34:12 -0500

    Improve pkey query

--- postgresql/select-primary-key.sql
+++ postgresql/select-primary-key.sql
@@ -1,7 +1,10 @@
-explain  select columns.column_name, columns.column_default, columns.is_nullable, columns.data_type
-   from information_schema.table_constraints tc                                                         
-   join information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name)                              
-   join information_schema.columns ON columns.table_schema = tc.constraint_schema AND tc.table_name = columns.table_name AND ccu.column_name = columns.column_name
-  where constraint_type = trim('PRIMARY KEY')
-    and tc.table_schema = trim('erm_experience')
-    and tc.table_name = trim('orders');
+select kc.column_name
+  from information_schema.table_constraints tc
+  join information_schema.key_column_usage kc on
+       kc.table_name = tc.table_name
+       and kc.table_schema = tc.table_schema
+       and kc.constraint_name = tc.constraint_name
+ where tc.constraint_type = trim('PRIMARY KEY')
+   and tc.table_schema = trim('public')
+   and tc.table_name = trim('inspector_table_1')
+ order by kc.ordinal_position;
